548b6e399c6bc0b4121230cb468951421b41e3b1
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-05-26 19:16:37 +0545

    Add component

---
+++ src/screens/component_screen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+const ComponentScreen = () => {
+    return <Text style={styles.textStyle}>Alisha at night learning</Text>;
+}
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 35
+    }
+})
+export default ComponentScreen;
