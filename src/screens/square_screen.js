d234ffe849cd1c9fb89df0669ca9cd5860746ec3
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-06-22 22:06:56 +0545

    addign community convention for action object with the use of type and payload

--- src/screens/square_screen.js
+++ src/screens/square_screen.js
@@ -1,52 +1,55 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 import ColorCounter from './components/color_counter'
 
 const COLOR_COUNTER = 15;
+//below action defines object that tells hpw i need to change my state object
+const reducer = (state, action) => {
+    //state == { red: number, green: number, blue: number, green: number}
+    //action === {colorToCHnage: 'red' || 'green' || 'blue', amount : 15|| -15}
+    //action === { type: 'change_red' || 'change_green' || 'change_blue' , payload: 15}
+    switch (action.type) {
+        case 'change_red':
+            return state.red + action.payload > 255 || state.red + action.payload < 0 ?
+                state :
+                { ...state, red: state.red + action.payload };
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0 ?
+                state :
+                { ...state, green: state.green + action.payload };
 
-const SquareScreen = () => {
-    const [red, setRed] = useState(0);
-    const [blue, setBlue] = useState(0);
-    const [green, setGreen] = useState(0);
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0 ?
+                state :
+                { ...state, blue: state.blue + action.payload };
 
-    //helper function 
-    const setColor = (color, change) => {
-        //color === 'red' , 'blue' , 'green'
-        //change === +15 , -15
-        switch (color) {
-            case 'red':
-                red + change > 255 || red + change < 0 ? null : setRed(red + change);
-                return;
-
-            case 'blue':
-                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
-                return;
-            case 'green':
-                green + change > 255 || green + change < 0 ? null : setGreen(green + change);
-                return;
-
-            default:
-                return;
-
-        }
+        default:
+            return state;
     }
 
-    console.log(red);
-    console.log(green);
-    console.log(blue);
+}
+const SquareScreen = () => {
+
+    const [state, runMyReducer] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+    console.log(state);
+    //destructuring
+    const { red, green, blue } = state;
+
+
+    //helper function 
     return (
         <View>
             <ColorCounter
-                onIncrease={() => setColor('red', COLOR_COUNTER)}
-                onDecrease={() => setColor('red', -1 * COLOR_COUNTER)}
+                onIncrease={() => runMyReducer({ type: 'change_red', payload: COLOR_COUNTER })}
+                onDecrease={() => runMyReducer({ type: 'change_red', payload: -COLOR_COUNTER })}
                 color='Red' />
             <ColorCounter
-                onIncrease={() => setColor('blue', + COLOR_COUNTER)}
-                onDecrease={() => setColor('blue', - 1 * COLOR_COUNTER)}
+                onIncrease={() => runMyReducer({ type: 'change_blue', payload: COLOR_COUNTER })}
+                onDecrease={() => runMyReducer({ type: 'change_blue', payload: -COLOR_COUNTER })}
                 color='Blue' />
             <ColorCounter
-                onIncrease={() => setColor('green', + COLOR_COUNTER)}
-                onDecrease={() => setColor('green', - 1 * COLOR_COUNTER)}
+                onIncrease={() => runMyReducer({ type: 'change_green', payload: COLOR_COUNTER })}
+                onDecrease={() => runMyReducer({ type: 'change_green', payload: -COLOR_COUNTER })}
                 color='Green' />
             <View
                 style={{
